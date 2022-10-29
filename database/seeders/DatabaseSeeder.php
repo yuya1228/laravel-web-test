abd643b3aca80b4a2dba1377905e0a5257f772b6
yoshioka <yussan5555@gmail.com>
2022-10-29 16:37:53 +0900

    laravel_web_test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(TodoSeeder::class);
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(TagTablesSeeder::class);
     }
 }
--- database/seeders/TagTablesSeeder.php
+++ database/seeders/TagTablesSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class TodoSeeder extends Seeder
+class TagTablesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,7 +14,7 @@ class TodoSeeder extends Seeder
      */
     public function run()
     {
-        $tags=[
+        $tag_id = [
             'id'=>'家事','勉強','運動','食事','移動'
         ];
     }
